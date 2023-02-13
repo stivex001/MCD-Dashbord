7c41dfd24f8ff2e910c9c307308e6e230dd4e00c
stivex001 <stephenadeyemo@gmail.com>
2023-02-13 16:46:32 +0100

    added styles to topbar component

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,16 +1,64 @@
 import { Search } from "@mui/icons-material";
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Wrapper = styled.div``;
-const LogoContainer = styled.div``;
-const Logo = styled.div``;
-const LogoImg = styled.img``;
-const LogoTitle = styled.span``;
-const BarSearch = styled.div``;
-const Input = styled.input``;
+const Container = styled.div`
+width: 100%;
+height: 50px;
+position: sticky;
+top: 0;
+left: 0;
+background-color:#605daf;
+`;
+const Wrapper = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 5px 20px;
+`;
+const LogoContainer = styled.div`
+display: flex;
+align-items: center;
+`;
+const Logo = styled.div`
+margin-right: 50px;
+display: flex;
+align-items: center;
+`;
+const LogoImg = styled.img`
+width: 20px;
+height: 20px;
+margin-right: 5px;
+`;
+const LogoTitle = styled.span`
+color: white;
+font-size: 700;
+`;
+const BarSearch = styled.div`
+display: flex;
+align-items: center;
+background-color: #7471b9;
+border-radius: 50px;
+border: 1px solid #605daf;
+padding: 5px 20px;
+cursor: pointer;
+`;
+const Input = styled.input`
+border: none;
+background-color: #7471b9;
+outline: none;
+color: white;
+
+::placeholder {
+  color: white;
+}
+`;
 const ImgContainer = styled.div``;
-const UserImg = styled.img``;
+const UserImg = styled.img`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+object-fit: cover;
+`;
 
 const Topbar = () => {
   return (
@@ -23,7 +71,7 @@ const Topbar = () => {
           </Logo>
           <BarSearch>
             <Input type="text" placeholder="Search..." />
-            <Search />
+            <Search style={{color: 'white', fontSize: '20px'}} />
           </BarSearch>
         </LogoContainer>
         <ImgContainer>
