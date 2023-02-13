8a606c0b7b39294ce476061cf031ef6de9cf27ca
stivex001 <stephenadeyemo@gmail.com>
2023-02-13 17:19:58 +0100

    fix: styling fixed

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,64 +1,16 @@
 import { Search } from "@mui/icons-material";
-import styled from "styled-components";
-
-const Container = styled.div`
-width: 100%;
-height: 50px;
-position: sticky;
-top: 0;
-left: 0;
-background-color:#605daf;
-`;
-const Wrapper = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 5px 20px;
-`;
-const LogoContainer = styled.div`
-display: flex;
-align-items: center;
-`;
-const Logo = styled.div`
-margin-right: 50px;
-display: flex;
-align-items: center;
-`;
-const LogoImg = styled.img`
-width: 20px;
-height: 20px;
-margin-right: 5px;
-`;
-const LogoTitle = styled.span`
-color: white;
-font-size: 700;
-`;
-const BarSearch = styled.div`
-display: flex;
-align-items: center;
-background-color: #7471b9;
-border-radius: 50px;
-border: 1px solid #605daf;
-padding: 5px 20px;
-cursor: pointer;
-`;
-const Input = styled.input`
-border: none;
-background-color: #7471b9;
-outline: none;
-color: white;
-
-::placeholder {
-  color: white;
-}
-`;
-const ImgContainer = styled.div``;
-const UserImg = styled.img`
-width: 40px;
-height: 40px;
-border-radius: 50%;
-object-fit: cover;
-`;
+import {
+  BarSearch,
+  Container,
+  ImgContainer,
+  Input,
+  Logo,
+  LogoContainer,
+  LogoImg,
+  LogoTitle,
+  UserImg,
+  Wrapper,
+} from "./topbar.styles";
 
 const Topbar = () => {
   return (
@@ -71,15 +23,18 @@ const Topbar = () => {
           </Logo>
           <BarSearch>
             <Input type="text" placeholder="Search..." />
-            <Search style={{color: 'white', fontSize: '20px'}} />
+            <Search style={{ color: "white", fontSize: "20px" }} />
           </BarSearch>
         </LogoContainer>
         <ImgContainer>
-          <UserImg src="https://avatars.githubusercontent.com/u/87942124?v=4" alt="" />
+          <UserImg
+            src="https://avatars.githubusercontent.com/u/87942124?v=4"
+            alt=""
+          />
         </ImgContainer>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
---
+++ src/components/Topbar/topbar.styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+height: 50px;
+position: sticky;
+top: 0;
+left: 0;
+background-color:#605daf;
+`;
+export const Wrapper = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 5px 20px;
+`;
+export const LogoContainer = styled.div`
+display: flex;
+align-items: center;
+`;
+export const Logo = styled.div`
+margin-right: 50px;
+display: flex;
+align-items: center;
+`;
+export const LogoImg = styled.img`
+width: 20px;
+height: 20px;
+margin-right: 5px;
+`;
+export const LogoTitle = styled.span`
+color: white;
+font-size: 700;
+`;
+export const BarSearch = styled.div`
+display: flex;
+align-items: center;
+background-color: #7471b9;
+border-radius: 50px;
+border: 1px solid #605daf;
+padding: 5px 20px;
+cursor: pointer;
+`;
+export const Input = styled.input`
+border: none;
+background-color: #7471b9;
+outline: none;
+color: white;
+
+::placeholder {
+  color: white;
+}
+`;
+export const ImgContainer = styled.div``;
+export const UserImg = styled.img`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+object-fit: cover;
+`;
