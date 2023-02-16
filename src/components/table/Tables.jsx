5be32a81acf9f553595b6148c27d832a791e3f31
stivex001 <stephenadeyemo@gmail.com>
2023-02-16 18:18:08 +0100

    implementing pagination on the tables

--- src/components/table/Tables.jsx
+++ src/components/table/Tables.jsx
@@ -7,6 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { rows } from "./tableData";
+import { DataGrid } from "@mui/x-data-grid";
+import { Bookmark } from "@mui/icons-material";
 
 const Container = styled.div``
 const Span = styled.span`
@@ -18,8 +20,111 @@ font-weight: bold;
 `;
 
 const Tables = () => {
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'username', headerName: 'Username', width: 100 },
+  { field: 'amount', headerName: 'Amount', type: 'number', width: 70 },
+  {
+    field: 'desc',
+    headerName: 'Description',
+    width: 200,
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    width: 100,
+  },
+  {
+    field: 'iwallet',
+    headerName: 'I.Wallet',
+    width: 100,
+  },
+  {
+    field: 'fwallet',
+    headerName: 'F.Wallet',
+    width: 100,
+  },
+  {
+    field: 'ip',
+    headerName: 'I.P',
+    width: 130,
+  },
+  {
+    field: 'server',
+    headerName: 'Server',
+    width: 70,
+  },
+  {
+    field: 'ref',
+    headerName: 'Ref',
+    width: 200,
+  },
+  {
+    field: 'date',
+    headerName: 'Date',
+    width: 130,
+  },
+  {
+    field: 'note',
+    headerName: 'Note',
+    width: 100,
+  },
+  {
+    field: 'action',
+    headerName: 'Action',
+    width: 130,
+    renderCell: () => {
+      return(
+        <>
+        <Bookmark />
+        </>
+      )
+    }
+  },
+  // {
+  //   field: 'status',
+  //   headerName: 'Status',
+  //   description: 'This column has a value getter and is not sortable.',
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params) =>
+  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  // },
+];
+
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
+
   return (
     <Container>
+           <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+
+    </Container>
+  )
+}
+
+export default Tables
+
+
+{/* <Container>
         <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
@@ -68,8 +173,4 @@ const Tables = () => {
       </Table>
     </TableContainer>
 
-    </Container>
-  )
-}
-
-export default Tables
+    </Container> */}
