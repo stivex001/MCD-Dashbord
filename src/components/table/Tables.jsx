cadc5ae9d9bd31a1728b44ba00bd5696ec7c968c
stivex001 <stephenadeyemo@gmail.com>
2023-02-16 15:16:59 +0100

    implemented background colors dynamicallylogicaly

--- src/components/table/Tables.jsx
+++ src/components/table/Tables.jsx
@@ -9,6 +9,13 @@ import Paper from '@mui/material/Paper';
 import { rows } from "./tableData";
 
 const Container = styled.div``
+const Span = styled.span`
+padding: 2.6px 4.2px;
+border-radius: 2px;
+color: #fff;
+font-size: 12px;
+font-weight: bold;
+`;
 
 const Tables = () => {
   return (
@@ -16,42 +23,45 @@ const Tables = () => {
         <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell>Username</TableCell>
-            <TableCell>Amount</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>I.Wallet</TableCell>
-            <TableCell>F.Wallet</TableCell>
-            <TableCell>I.P </TableCell>
-            <TableCell>Server</TableCell>
-            <TableCell>Ref</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Note</TableCell>
-            <TableCell>Action</TableCell>
+          <TableRow style={{backgroundColor: '#f3f2f7'}}>
+            <TableCell style={{color: '#8281cc'}}>id</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Username</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Amount</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Description</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Status</TableCell>
+            <TableCell style={{color: '#8281cc'}}>I.Wallet</TableCell>
+            <TableCell style={{color: '#8281cc'}}>F.Wallet</TableCell>
+            <TableCell style={{color: '#8281cc'}}>I.P </TableCell>
+            <TableCell style={{color: '#8281cc'}}>Server</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Ref</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Date</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Note</TableCell>
+            <TableCell style={{color: '#8281cc'}}>Action</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
               key={row.id}
+              style={{backgroundColor: '#f3f2f7'}}
             >
-              <TableCell>
+              <TableCell style={{color: '#8887a9'}}>
                 {row.id}
               </TableCell>
-              <TableCell>{row.username}</TableCell>
-              <TableCell>{row.amount}</TableCell>
-              <TableCell>{row.desc}</TableCell>
-              <TableCell>{row.status}</TableCell>
-              <TableCell>{row.iwallet}</TableCell>
-              <TableCell>{row.fwallet}</TableCell>
-              <TableCell>{row.ip}</TableCell>
-              <TableCell>{row.server}</TableCell>
-              <TableCell>{row.ref}</TableCell>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.note}</TableCell>
-              <TableCell>{row.action}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.username}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.amount}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.desc}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>
+                <Span style={{backgroundColor: `${row.status === 'successful' ? '#33cdff' : '#5dd099'}`}}>{row.status}</Span>
+              </TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.iwallet}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.fwallet}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.ip}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.server}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.ref}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.date}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.note}</TableCell>
+              <TableCell style={{color: '#8887a9'}}>{row.action}</TableCell>
             </TableRow>
           ))}
         </TableBody>
