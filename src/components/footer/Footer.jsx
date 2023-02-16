213b94ad7afe4a011a2faccd129e1d9a445bfcc3
stivex001 <stephenadeyemo@gmail.com>
2023-02-16 22:49:56 +0100

    fix: fixed code formatiing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,24 +1,21 @@
 import styled from "styled-components";
 
 const Container = styled.footer`
-background-color: #605daf05;
-padding: 20px 30px;
-text-align: center;
+  background-color: #605daf05;
+  padding: 20px 30px;
+  text-align: center;
 `;
 const Wrapper = styled.span`
-color: #2d3b48;
-font-size: 14px;
-
+  color: #2d3b48;
+  font-size: 14px;
 `;
 
 const Footer = () => {
   return (
     <Container>
-        <Wrapper>
-        &copy; 2020 Mega Cheap Data by 5Star Inn Company.
-        </Wrapper>
+      <Wrapper>&copy; 2020 Mega Cheap Data by 5Star Inn Company.</Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
