b3402a8484e1beee298ba6231d5721e737a8c9a0
stivex001 <stephenadeyemo@gmail.com>
2023-02-16 22:49:12 +0100

    feat:implemented footer components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+background-color: #605daf05;
+padding: 20px 30px;
+text-align: center;
+`;
+const Wrapper = styled.span`
+color: #2d3b48;
+font-size: 14px;
+
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+        <Wrapper>
+        &copy; 2020 Mega Cheap Data by 5Star Inn Company.
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Footer
--- src/pages/transaction/TransHistory.jsx
+++ src/pages/transaction/TransHistory.jsx
@@ -1,63 +1,66 @@
-import React from 'react'
-import styled from 'styled-components';
-import Card from '../../components/card/Card';
-import Topbar from '../../components/Topbar/Topbar'
-import TransactionTables from '../../components/Transactiontables/TransactionTables';
-import { mobile, tablet } from '../../responsive';
+import React from "react";
+import styled from "styled-components";
+import Card from "../../components/card/Card";
+import Footer from "../../components/footer/Footer";
+import Topbar from "../../components/Topbar/Topbar";
+import TransactionTables from "../../components/Transactiontables/TransactionTables";
+import { mobile, tablet } from "../../responsive";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
-padding: 20px;
+  padding: 20px;
 `;
 const Desc = styled.div`
-display: flex;
-align-items: center;
-justify-content: space-between;
-margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
 `;
 const H3 = styled.h3`
-font-size: 18px;
-color: #4a4b4c;
-${mobile({ display: "none"})};
-
+  font-size: 18px;
+  color: #4a4b4c;
+  ${mobile({ display: "none" })};
 `;
 const DescP = styled.p`
-font-size: 14px;
-color: #8c9ea9;
+  font-size: 14px;
+  color: #8c9ea9;
 `;
 const DescSpan = styled.span`
-font-size: 14px;
-color: #6c757d;
+  font-size: 14px;
+  color: #6c757d;
 `;
 const CardContainer = styled.div`
-display: flex;
-justify-content: space-between;
-gap: 20px;
-margin-bottom: 50px;
-${tablet({ flexDirection: "column" })};
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  margin-bottom: 50px;
+  ${tablet({ flexDirection: "column" })};
 `;
 
 const TransHistory = () => {
   return (
     <Container>
-        <Topbar />
-        <Wrapper>
+      <Topbar />
+      <Wrapper>
         <Desc>
-                <H3>Transaction List</H3>
-                <DescP>Transaction / <DescSpan>Transactions List</DescSpan></DescP>
-            </Desc>
-            <CardContainer>
-              <Card type='totalTransactions' />
-              <Card type='transactionToday' />
-              <Card type='transactionYesterday' />
-              <Card type='transactionAgo' />
-              </CardContainer>
+          <H3>Transaction List</H3>
+          <DescP>
+            Transaction / <DescSpan>Transactions List</DescSpan>
+          </DescP>
+        </Desc>
+        <CardContainer>
+          <Card type="totalTransactions" />
+          <Card type="transactionToday" />
+          <Card type="transactionYesterday" />
+          <Card type="transactionAgo" />
+        </CardContainer>
 
-              {/* TABLES */}
-              <TransactionTables />
-        </Wrapper>
+        {/* TABLES */}
+        <TransactionTables />
+      </Wrapper>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default TransHistory
+export default TransHistory;
