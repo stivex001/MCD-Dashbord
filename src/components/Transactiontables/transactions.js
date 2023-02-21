cd7de14ec37ac6b670ec142e5b31313f43cdd8c7
stivex001 <stephenadeyemo@gmail.com>
2023-02-21 01:00:06 +0100

    feat:iadded styles to the table component

--- src/components/Transactiontables/transactions.js
+++ src/components/Transactiontables/transactions.js
@@ -236,3 +236,92 @@ export const marketData = [
 
   ];
   
+  
+  export const chargesData = [
+    {
+      id: 1,
+      gl: "Personal Account",
+      narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+      amount: "#5.87",
+      type: "expenses",
+      date: "2023-02-17 08:52:48",
+    },
+    {
+        id: 2,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "income",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 3,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "expenses",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 4,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "income",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 5,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "expenses",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 6,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "income",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 7,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "expenses",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 8,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "expenses",
+        phone: "08162174754",
+        staus: "Pending",
+        date: "2023-02-17 08:52:48",
+        receiver: "wallet",
+        version: '6.5.6'
+      },
+      {
+        id: 9,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "expenses",
+        date: "2023-02-17 08:52:48",
+      },
+      {
+        id: 10,
+        gl: "Personal Account",
+        narration: "Payment gateway charges on personal account with ref MNFY|25|202302130579288|195734",
+        amount: "#5.87",
+        type: "expenses",
+        date: "2023-02-17 08:52:48",
+      },
+
+  ];
+  
--- src/pages/transaction/PCharges.jsx
+++ src/pages/transaction/PCharges.jsx
@@ -1,116 +1,102 @@
 import {
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableRow,
-  } from "@mui/material";
-  import styled from "styled-components";
-  import Footer from "../../components/footer/Footer";
-  import Topbar from "../../components/Topbar/Topbar";
-  import { marketData } from "../../components/Transactiontables/transactions";
-  import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
-  
-  const Container = styled.div``;
-  const Wrapper = styled.div`
-    padding: 20px;
-  `;
-  const TableWrapper = styled.div`
-    background-color: #fff;
-    box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-    -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-    -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
-    padding: 20px 30px;
-  `;
-  const Details = styled.div`
-    margin: 30px 0;
-  `;
-  const Span = styled.span`
-    color: #fff;
-    padding: 2.6px 4.2px;
-    font-size: 10.5px;
-    border-radius: 5px;
-  `;
-  
-  const PCharges = () => {
-    return (
-      <Container>
-        <Topbar />
-        <Wrapper>
-          <Desc>
-            <H3>General Market History</H3>
-            <DescP>
-              Transaction / <DescSpan>General Market History</DescSpan>
-            </DescP>
-          </Desc>
-        </Wrapper>
-        <TableWrapper>
-          <Details>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from "@mui/material";
+import styled from "styled-components";
+import Footer from "../../components/footer/Footer";
+import Topbar from "../../components/Topbar/Topbar";
+import {
+  chargesData,
+} from "../../components/Transactiontables/transactions";
+import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+const TableWrapper = styled.div`
+  background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px 30px;
+`;
+const Details = styled.div`
+  margin: 30px 0;
+`;
+const Span = styled.span`
+  color: #fff;
+  padding: 2.6px 4.2px;
+  font-size: 10.5px;
+  border-radius: 5px;
+`;
+
+const PCharges = () => {
+  return (
+    <Container>
+      <Topbar />
+      <Wrapper>
+        <Desc>
+          <H3>Charges History</H3>
+          <DescP>
+            Transaction / <DescSpan>Charges History</DescSpan>
+          </DescP>
+        </Desc>
+      </Wrapper>
+      <TableWrapper>
+        <Details>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableBody>
+                <TableRow style={{ backgroundColor: "#f3f2f7" }}>
+                  <TableCell style={{ color: "#8887a9" }}>id</TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>GL</TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>Type</TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>Amount</TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>Narration</TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>Date</TableCell>
+                </TableRow>
+              </TableBody>
+              {chargesData.map((row) => (
                 <TableBody>
-                  <TableRow style={{ backgroundColor: "#f3f2f7" }}>
-                    <TableCell style={{ color: "#8887a9" }}>id</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>Username</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>Amount</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>Type</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>Trans ID</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>O.Balance</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>N.Balance</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>Version</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>Date</TableCell>
+                  <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
+                    <TableCell style={{ color: "#8887a9" }}>{row.id}</TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>{row.gl}</TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      <Span
+                        style={{
+                          backgroundColor: `${
+                            row.type === "income" ? "#5dd099" : "#f8c955"
+                          }`,
+                        }}
+                      >
+                        {row.type}
+                      </Span>
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.amount}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.narration}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.date}
+                    </TableCell>
                   </TableRow>
                 </TableBody>
-                {marketData.map((row) => (
-                  <TableBody>
-                    <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
-                      <TableCell style={{ color: "#8887a9" }}>{row.id}</TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.username}
-                      </TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.amount}
-                      </TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        <Span
-                          style={{
-                            backgroundColor: `${
-                              row.type === "Credit" ? "#5dd099" : "#f8c955"
-                            }`,
-                          }}
-                        >
-                          {row.type}
-                        </Span>
-                      </TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.transId}
-                      </TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.oBalance}
-                      </TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.nBalance}
-                      </TableCell>
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.version}
-                      </TableCell>
-  
-                      <TableCell style={{ color: "#8887a9" }}>
-                        {row.date}
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                ))}
-              </Table>
-            </TableContainer>
-          </Details>
-        </TableWrapper>
-  
-        <Footer />
-      </Container>
-    );
-  };
-  
-  export default PCharges;
-  
+              ))}
+            </Table>
+          </TableContainer>
+        </Details>
+      </TableWrapper>
+
+      <Footer />
+    </Container>
+  );
+};
+
+export default PCharges;
