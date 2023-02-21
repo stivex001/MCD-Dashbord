46813900d95945eebf8cce6cc6043f2e1f7dc2b8
stivex001 <stephenadeyemo@gmail.com>
2023-02-22 00:27:56 +0100

    fix:typo fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,11 @@
 import styled from "styled-components";
-import Sidebar from "../../components/Sidebar/Sidebar";
 import Topbar from "../../components/Topbar/Topbar";
 import Widgets from "../../components/widgets/Widgets";
 
 const Container = styled.div`
   // display: flex;
 `;
-const TopbarContainer = styled.div`
-  display: flex;
-`;
-const WidgetContainer = styled.div`
-  flex: 4;
-`;
+
 
 const Home = () => {
   return (
