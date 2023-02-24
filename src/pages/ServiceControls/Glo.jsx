ef4355e5252b04b8b3642f513223763f13ba5117
stivex001 <stephenadeyemo@gmail.com>
2023-02-24 12:00:31 +0100

    fix: typo fixed

--- src/pages/ServiceControls/Glo.jsx
+++ src/pages/ServiceControls/Glo.jsx
@@ -7,7 +7,9 @@ import Tablehead from "../../components/DataPlansTable/Tablehead";
 import Footer from "../../components/footer/Footer";
 import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
 
-const Container = styled.div``;
+const Container = styled.div`
+margin: 70px 0;
+`;
 const Wrapper = styled.div`
   padding: 20px;
 `;
