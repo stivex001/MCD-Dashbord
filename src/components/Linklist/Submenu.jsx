5bf0e150de6caf549722a6578cc8b646ee5d1321
stivex001 <stephenadeyemo@gmail.com>
2023-02-26 22:13:04 +0100

    fix: code refactored

--- src/components/Linklist/Submenu.jsx
+++ src/components/Linklist/Submenu.jsx
@@ -1,45 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const Container = styled.div``;
-const SidebarLink = styled(Link)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-  list-style: none;
-  height: 60px;
-  text-decoration: none;
-  font-size: 18px;
-
-  &:hover {
-    color: gray;
-    cursor: pointer;
-  }
-`;
-const SidebarLabel = styled.span`
-  margin-left: 16px;
-  color: #2d3b48;
-  font-size: 14px;
-
-  &:hover {
-    color: #605daf;
-  }
-`;
-const DropdownLink = styled(Link)`
-  height: 60px;
-  padding-left: 3rem;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  color: #f5f5f5;
-  font-size: 18px;
-`;
-const SidebarItem = styled.div`
-  display: flex;
-  align-items: center;
-`;
+import {
+  Container,
+  DropdownLink,
+  SidebarItem,
+  SidebarLabel,
+  SidebarLink,
+} from "./subNenustyles";
 
 const Submenu = ({ item }) => {
   const [subNav, setSubnav] = useState(false);
---
+++ src/components/Linklist/subNenustyles.js
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const SidebarLink = styled(Link)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+
+  &:hover {
+    color: gray;
+    cursor: pointer;
+  }
+`;
+export const SidebarLabel = styled.span`
+  margin-left: 16px;
+  color: #2d3b48;
+  font-size: 14px;
+
+  &:hover {
+    color: #605daf;
+  }
+`;
+export const DropdownLink = styled(Link)`
+  height: 60px;
+  padding-left: 3rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #f5f5f5;
+  font-size: 18px;
+`;
+export const SidebarItem = styled.div`
+  display: flex;
+  align-items: center;
+`;
