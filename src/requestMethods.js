eb7ddc2d8c36aa164ffff0a1f01d6230d471c50c
stivex001 <stephenadeyemo@gmail.com>
2023-03-02 02:40:48 +0100

    typo fixed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://adminapi.mcd.5starcompany.com.ng/api/v1";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.token;
-
+// const TOKEN = ''
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
