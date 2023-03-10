339148a992295448ec19f3b54f2359236fb24ed5
stivex001 <stephenadeyemo@gmail.com>
2023-03-10 20:09:53 +0100

    implementing dynamic link

---
+++ src/components/General/General.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px;
+  margin: 50px 0;
+`;
+
+const General = ({showGeneral}) => {
+  return (
+    <Container>General</Container>
+  )
+}
+
+export default General
