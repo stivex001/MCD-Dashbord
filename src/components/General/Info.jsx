9f1f9dbc57c67048e26ce7554edd486f441f157d
stivex001 <stephenadeyemo@gmail.com>
2023-03-11 15:39:19 +0100

    feat:implementing slider components

--- src/components/General/Info.jsx
+++ src/components/General/Info.jsx
@@ -1,41 +1,69 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { sliderData } from "./slideData";
 
-const Container = styled.div``;
-const MessageWrapper = styled.div`
+const Container = styled.div`
 display: flex;
-flex-direction: column;
-gap: 10px;
+overflow: hidden;
+position: relative;
+
+`;
+const MessageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100vw;
 `;
 const Message = styled.p`
-font-size: 24px;
-color: ${props => props.type === 'alert' ? '#4a4b4c' : '#2d3b48'};
-font-weight: bold;
+  font-size: 24px;
+  color: ${(props) => (props.type === "alert" ? "#4a4b4c" : "#2d3b48")};
+  font-weight: bold;
 `;
 const Span = styled.span`
-font-size: 14px;
-color: #8c9ea9;
+  font-size: 14px;
+  color: #8c9ea9;
 `;
 
 const Info = () => {
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoScroll) {
+      const auto = () => {
+        slideInterval = setInterval(nextSlide, intervalTime);
+      };
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentSlide, slideInterval, autoScroll]);
+
   return (
     <Container>
+      {sliderData.map((slide, index) => (
         <MessageWrapper>
-            <Message type='alert'>
-        Make up to 10 transactions to be eligible for an Agent and send a
-        request mail to info@5startcompany.com.ng where you earn incentives on
-        transactions done at the end of the month
-      </Message>
-      <Span>Target</Span>
+          <Message>
+            {slide.desc}
+          </Message>
+          <Span>{slide.span}</Span>
         </MessageWrapper>
-      <MessageWrapper>
-        <Message>
-        To Load MTN (Data Pin 1.5GB). Dial: *460*6*1#, then enter pin Generated
-        in the prompt shown on your MTN sim. To Check the Data Balance Dial:
-        *312*5#
-      </Message>
-      <Span>General News</Span>
-      </MessageWrapper>
-      
+      ))}
     </Container>
   );
 };
---
+++ src/components/General/slideData.js
@@ -0,0 +1,10 @@
+export const sliderData = [
+  {
+    desc: "Make up to 10 transactions to be eligible for an Agent and send a request mail to info@5startcompany.com.ng where you earn incentives on transactions done at the end of the month",
+    span: "Target",
+  },
+  {
+    desc: "To Load MTN (Data Pin 1.5GB). Dial: *460*6*1#, then enter pin Generated in the prompt shown on your MTN sim. To Check the Data Balance Dial:*312*5#",
+    span: "General News",
+  },
+];
