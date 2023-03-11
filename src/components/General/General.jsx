f0d0840243d32f540595d8f800d53b162fcc5b80
stivex001 <stephenadeyemo@gmail.com>
2023-03-12 00:45:28 +0100

    fix: code refactored

--- src/components/General/General.jsx
+++ src/components/General/General.jsx
@@ -5,11 +5,11 @@ import Performance from "./Performance";
 import Widget from "./WIdget";
 
 const Container = styled.div`
-display: flex;
-gap: 30px;
-width: 100%;
-height: 100%;
-${tablet({ flexDirection: "column", gap: '0'})};
+  display: flex;
+  gap: 30px;
+  width: 100%;
+  height: 100%;
+  ${tablet({ flexDirection: "column", gap: "0" })};
 `;
 const Wrapper = styled.div`
   background-color: #fff;
@@ -18,17 +18,17 @@ const Wrapper = styled.div`
   -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
   padding: 20px;
   margin: 50px 0;
-  width: ${prop => prop.width};
+  width: ${(prop) => prop.width};
 `;
 const Left = styled.div`
-flex: 1.2;
+  flex: 1.2;
 `;
 const Right = styled.div`
-flex: 2;
+  flex: 2;
 `;
 const Ref = styled.p`
-color:#333265;
-font-size: 14px;
+  color: #333265;
+  font-size: 14px;
 `;
 
 const General = () => {
@@ -43,7 +43,7 @@ const General = () => {
         </Wrapper>
       </Left>
       <Right>
-        <Wrapper width='70%'>
+        <Wrapper width="70%">
           <Performance />
         </Wrapper>
         <Ref>Referrals:</Ref>
