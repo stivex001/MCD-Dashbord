2a2e0383e3113247b10511771ae3ef23c0207b1c
stivex001 <stephenadeyemo@gmail.com>
2023-03-11 18:19:35 +0100

    code refactored

--- src/components/General/Info.jsx
+++ src/components/General/Info.jsx
@@ -3,16 +3,20 @@ import styled from "styled-components";
 import { sliderData } from "./slideData";
 
 const Container = styled.div`
-display: flex;
-overflow: hidden;
-position: relative;
-
+height: 30vh;
+  overflow: hidden;
+  position: relative;
 `;
 const MessageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100vw;
+  height: ${(props) => props.numPages * 30}vh;
+//   position: absolute;
+  top: ${(props) => -props.currentPage * 30}vh;
+  transition: transform 1s ease-in-out;
+  transform: translateY(${(props) => -props.currentPage * 30}vh);
+    // display: flex;
+  //   flex-direction: column;
+  //   gap: 10px;
+  //   width: 100vw;
 `;
 const Message = styled.p`
   font-size: 24px;
@@ -24,43 +28,34 @@ const Span = styled.span`
   color: #8c9ea9;
 `;
 
-const Info = () => {
-  const autoScroll = true;
-  let slideInterval;
-  let intervalTime = 5000;
+const Info = ({ numPages }) => {
+//   const autoScroll = true;
+//   let slideInterval;
+//   let intervalTime = 5000;
 
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const slideLength = sliderData.length;
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const nextSlide = () => {
-    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
-  };
+//   const nextSlide = () => {
+//     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+//   };
 
-  const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
-  };
+//   const prevSlide = () => {
+//     setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+//   };
 
-  useEffect(() => {
-    setCurrentSlide(0);
-  }, []);
+useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPage((currentPage) => (currentPage + 1) % numPages);
+    }, 3000);
+    return () => clearInterval(interval);
+}, [numPages]);
 
-  useEffect(() => {
-    if (autoScroll) {
-      const auto = () => {
-        slideInterval = setInterval(nextSlide, intervalTime);
-      };
-      auto();
-    }
-    return () => clearInterval(slideInterval);
-  }, [currentSlide, slideInterval, autoScroll]);
 
   return (
-    <Container>
+    <Container numPages={numPages} currentPage={currentPage}>
       {sliderData.map((slide, index) => (
-        <MessageWrapper>
-          <Message>
-            {slide.desc}
-          </Message>
+        <MessageWrapper key={index}>
+          <Message>{slide.desc}</Message>
           <Span>{slide.span}</Span>
         </MessageWrapper>
       ))}
