cd0da01fd1f673c62f3d6aad2c6918e5e2191c9b
stivex001 <stephenadeyemo@gmail.com>
2023-03-13 11:25:10 +0100

    implemeting push notification page

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,17 +1,61 @@
+import { NearMe } from "@mui/icons-material";
 import styled from "styled-components";
 
 const Container = styled.div`
+  margin: 50px 0;
+  display: flex;
+  gap: 30px;
+`;
+const Left = styled.div`
+flex: 1;
 background-color: #fff;
   box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
   padding: 20px;
-  margin: 50px 0;
+`;
+const Textarea = styled.textarea`
+padding: 6px 12px;
+outline-color: #333265;
+`;
+const Btn = styled.button`
+display: flex;
+align-items: center;
+margin: 5px 0 20px;
+padding: 6px 12px;
+color: #fff;
+background-color: #5b63ae;
+cursor: pointer;
+border: none;
+border-radius: 2px;
+&:hover {
+  color: black;
+  transition: all 0.5s;
+}
+`;
+const Right = styled.div`
+flex:3;
+background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px;
 `;
 
 const Nofication = () => {
   return (
-    <Container>Nofication</Container>
+    <Container>
+      <Left>
+        <Textarea placeholder="Message" />
+        <Btn>
+          <NearMe />
+          Send Message
+        </Btn>
+      </Left>
+      <Right>
+        
+      </Right>
+    </Container>
   )
 }
 
