f1f012c84cf09fa33d7e6f2284359976ff9efeb7
stivex001 <stephenadeyemo@gmail.com>
2023-03-14 13:36:05 +0100

    fix: changed the input font-weight

--- src/pages/Settings/Edit.jsx
+++ src/pages/Settings/Edit.jsx
@@ -40,6 +40,7 @@ const Input = styled.input`
   padding: 12px;
   flex: 1;
   outline: none;
+  font-weight: 300;
   background-color: ${(prop) =>
     prop.background === "email" ? "#e9ecef" : "none"};
   border: 0.5px solid #d4d8e1;
