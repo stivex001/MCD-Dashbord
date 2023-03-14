92da76c51089f7580ebcd175dd75de67f089cfa6
stivex001 <stephenadeyemo@gmail.com>
2023-03-14 12:38:56 +0100

    fix: code refactored

--- src/pages/Settings/Edit.jsx
+++ src/pages/Settings/Edit.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Navbar from "../../components/Bar/Navbar";
 import Footer from "../../components/footer/Footer";
@@ -11,17 +14,21 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 20px;
   height: 73vh;
+  background-color: #f5f5f5;
 `;
 const Form = styled.form`
-background-color: #fff;
+  background-color: #fff;
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-bottom: 10px;
+`;
+const FormWrapper = styled.div`
   box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
   padding: 20px 30px;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  margin-bottom: 50px;
 `;
 const InputContainer = styled.div`
   display: flex;
@@ -33,11 +40,12 @@ const Input = styled.input`
   padding: 12px;
   flex: 1;
   outline: none;
-  background-color: ${prop => prop.background === 'email' ? '#e9ecef' : 'none'};
+  background-color: ${(prop) =>
+    prop.background === "email" ? "#e9ecef" : "none"};
   border: 0.5px solid #d4d8e1;
 
   &:focus {
-    outline:0.1px solid #605daf;
+    outline: 0.1px solid #605daf;
   }
 `;
 const InputTitle = styled.p`
@@ -56,7 +64,6 @@ const Btn = styled.button`
   cursor: pointer;
   border: none;
   border-radius: 5px;
-  width: 10%;
   text-align: center;
   &:hover {
     color: black;
@@ -65,6 +72,21 @@ const Btn = styled.button`
 `;
 
 const Edit = () => {
+  const location = useLocation();
+  const settingsId = Number(location.pathname.split("/")[2]);
+  const [inputData, setInputData] = useState({
+    name: "",
+    message: "",
+  });
+
+  const settings = useSelector((state) =>
+    state.settings.settings.find((setting) => setting.id === settingsId)
+  );
+
+  const handleData = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
+
   return (
     <Container>
       <Navbar />
@@ -75,17 +97,30 @@ const Edit = () => {
             Settings / <DescSpan>Modify Setting</DescSpan>
           </DescP>
         </Desc>
-        <Form>
-          <InputContainer>
-            <InputTitle>Name</InputTitle>
-            <Input background='email'  value='iguegiugujbjkbjkb' />
-          </InputContainer>
-          <InputContainer>
-            <InputTitle>Value</InputTitle>
-            <Input type="text" placeholder="" />
-          </InputContainer>
+        <FormWrapper>
+          <Form>
+            <InputContainer>
+              <InputTitle>Name</InputTitle>
+              <Input
+                background="email"
+                onChange={handleData}
+                value={inputData.name || ""}
+                name="name"
+                placeholder={settings.name}
+              />
+            </InputContainer>
+            <InputContainer>
+              <InputTitle>Value</InputTitle>
+              <Input
+                type="text"
+                onChange={handleData}
+                value={inputData.message || ""}
+                name="name"
+              />
+            </InputContainer>
+          </Form>
           <Btn>Update</Btn>
-        </Form>
+        </FormWrapper>
       </Wrapper>
       <Footer />
     </Container>
