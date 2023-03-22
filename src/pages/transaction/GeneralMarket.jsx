34580e544c7b388823f72d94668dcf5410481d0c
stivex001 <stephenadeyemo@gmail.com>
2023-03-22 16:04:48 +0100

    fix: code refactored

--- src/pages/transaction/GeneralMarket.jsx
+++ src/pages/transaction/GeneralMarket.jsx
@@ -6,39 +6,22 @@ import {
   TableContainer,
   TableRow,
 } from "@mui/material";
-import styled from "styled-components";
 import Navbar from "../../components/Bar/Navbar";
 import Footer from "../../components/footer/Footer";
 import { marketData } from "../../components/Transactiontables/transactions";
 import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
-
-const Container = styled.div`
-margin: 70px 0;
-`;
-const Wrapper = styled.div`
-  padding: 20px;
-`;
-const TableWrapper = styled.div`
-  background-color: #fff;
-  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
-  padding: 20px 30px;
-`;
-const Details = styled.div`
-  margin: 30px 0;
-`;
-const Span = styled.span`
-  color: #fff;
-  padding: 2.6px 4.2px;
-  font-size: 10.5px;
-  border-radius: 5px;
-`;
+import {
+  Container,
+  Details,
+  Span,
+  TableWrapper,
+  Wrapper,
+} from "./general.styles";
 
 const GeneralMarket = () => {
   return (
     <Container>
-      <Navbar/>
+      <Navbar />
       <Wrapper>
         <Desc>
           <H3>General Market History</H3>
@@ -46,69 +29,94 @@ const GeneralMarket = () => {
             Transaction / <DescSpan>General Market History</DescSpan>
           </DescP>
         </Desc>
-      </Wrapper>
-      <TableWrapper>
-        <Details>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableBody>
-                <TableRow style={{ backgroundColor: "#f3f2f7" }}>
-                  <TableCell style={{ color: "#8887a9" }}>id</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Username</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Amount</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Type</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Trans ID</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>O.Balance</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>N.Balance</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Version</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Date</TableCell>
-                </TableRow>
-              </TableBody>
-              {marketData.map((row) => (
+        <TableWrapper>
+          <Details>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableBody>
-                  <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
-                    <TableCell style={{ color: "#8887a9" }}>{row.id}</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.username}
+                  <TableRow style={{ backgroundColor: "#f3f2f7" }}>
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      id
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.amount}
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      Username
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      <Span
-                        style={{
-                          backgroundColor: `${
-                            row.type === "Credit" ? "#5dd099" : "#f8c955"
-                          }`,
-                        }}
-                      >
-                        {row.type}
-                      </Span>
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      Amount
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.transId}
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      Type
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.oBalance}
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      Trans ID
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.nBalance}
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      O.Balance
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.version}
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      N.Balance
                     </TableCell>
-
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.date}
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      Version
+                    </TableCell>
+                    <TableCell style={{ color: "#827fc0", fontWeight: "bold" }}>
+                      Date
                     </TableCell>
                   </TableRow>
                 </TableBody>
-              ))}
-            </Table>
-          </TableContainer>
-        </Details>
-      </TableWrapper>
+                {marketData.map((row) => (
+                  <TableBody>
+                    <TableRow
+                      key={row.id}
+                      style={{
+                        backgroundColor:
+                          row.id % 2 === 0 ? "#ffffff" : "#f3f2f7",
+                      }}
+                    >
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.id}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.username}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.amount}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        <Span
+                          style={{
+                            backgroundColor: `${
+                              row.type === "Credit" ? "#5dd099" : "#f8c955"
+                            }`,
+                          }}
+                        >
+                          {row.type}
+                        </Span>
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.transId}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.oBalance}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.nBalance}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.version}
+                      </TableCell>
 
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.date}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                ))}
+              </Table>
+            </TableContainer>
+          </Details>
+        </TableWrapper>
+      </Wrapper>
       <Footer />
     </Container>
   );
---
+++ src/pages/transaction/general.styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { laptop } from "../../responsive";
+
+export const Container = styled.div`
+margin: 70px 0;
+${laptop({ marginLeft: "250px" })};
+`;
+export const Wrapper = styled.div`
+  padding: 20px;
+`;
+export const TableWrapper = styled.div`
+  background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 10px 20px;
+`;
+export const Details = styled.div`
+  margin: 30px 0;
+`;
+export const Span = styled.span`
+  color: #fff;
+  padding: 2.6px 4.2px;
+  font-size: 10.5px;
+  border-radius: 5px;
+`;
