22e25cbf3d13600082bd1d17062000f8f8f1d4b0
stivex001 <stephenadeyemo@gmail.com>
2023-03-24 17:29:40 +0100

    fix: code refactored

--- src/components/Transactiontables/ReversalTrans.jsx
+++ src/components/Transactiontables/ReversalTrans.jsx
@@ -1,28 +1,63 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-margin: 50px 0;
-display: flex;
-
-`;
-const Left = styled.div`
-flex: 1;
-`;
-const Right = styled.div`
-flex: 1;
-`;
+import {
+  Container,
+  Left,
+  Right,
+  TableBodys,
+  TableCellB,
+  TableCells,
+  TableContain,
+  TableHeads,
+  TableRows,
+  Title,
+} from "./reversal.styles";
 
 const ReversalTrans = () => {
   return (
     <Container>
-        <Left>
-            <p>Transaction</p>
-        </Left>
-        <Right>
-            <p>Reversal</p>
-        </Right>
+      <Left>
+        <Title>Transaction</Title>
+        <TableContain>
+          <TableHeads>
+            <TableRows style={{ fontWeight: "bold" }}>
+              <TableCells>#</TableCells>
+              <TableCells>Description</TableCells>
+              <TableCells>Amount</TableCells>
+              <TableCells>Username</TableCells>
+            </TableRows>
+          </TableHeads>
+          <TableBodys>
+            <TableCellB>1</TableCellB>
+            <TableCellB>
+              odexcheapdata fund wallet using bank transfer to GTBank
+            </TableCellB>
+            <TableCellB>#1000</TableCellB>
+            <TableCellB>odexcheapdata</TableCellB>
+          </TableBodys>
+        </TableContain>
+      </Left>
+      <Right>
+        <Title>Reversal</Title>
+        <TableContain>
+          <TableHeads>
+            <TableRows style={{ fontWeight: "bold" }}>
+              <TableCells>#</TableCells>
+              <TableCells>Description</TableCells>
+              <TableCells>Amount</TableCells>
+              <TableCells>Username</TableCells>
+            </TableRows>
+          </TableHeads>
+          <TableBodys>
+            <TableCellB>1</TableCellB>
+            <TableCellB>
+              odexcheapdata fund wallet using bank transfer to GTBank
+            </TableCellB>
+            <TableCellB>#1000</TableCellB>
+            <TableCellB>odexcheapdata</TableCellB>
+          </TableBodys>
+        </TableContain>
+      </Right>
     </Container>
-  )
-}
+  );
+};
 
-export default ReversalTrans
+export default ReversalTrans;
---
+++ src/components/Transactiontables/reversal.styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { mobile } from "../../responsive";
+
+export const Container = styled.div`
+  margin: 50px 0;
+  display: flex;
+  gap: 50px;
+  ${mobile({ flexDirection: "columns" })};
+`;
+export const Left = styled.div`
+  flex: 1;
+  background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px 30px;
+`;
+export const Right = styled.div`
+  flex: 1;
+  background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px 30px;
+`;
+export const Title = styled.p`
+  font-size: 13px;
+  color: #333265;
+`;
+export const TableContain = styled.table`
+  margin: 20px 0;
+  border-collapse: collapse;
+  width: 100%;
+`;
+
+export const TableHeads = styled.thead`
+  background-color: #f3f2f7;
+  border: 1px solid #ddd;
+`;
+
+export const TableBodys = styled.tbody`
+  background-color: #f6f6f9;
+`;
+
+export const TableRows = styled.tr`
+  &:nth-child(even) {
+    background-color: #f3f2f7;
+  }
+`;
+export const TableCells = styled.td`
+  padding: 12px;
+  border: none;
+  color: #8281cc;
+  font-size: 14px;
+`;
+export const TableCellB = styled.td`
+  color: #8887a9;
+  padding: 14px 12px;
+  font-size: 14px;
+`;
