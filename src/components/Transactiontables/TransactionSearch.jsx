29c84aec11c02e4b30b382ffaaf6fa429402929e
stivex001 <stephenadeyemo@gmail.com>
2023-03-24 14:22:34 +0100

    feat: implemented search transaction page

---
+++ src/components/Transactiontables/TransactionSearch.jsx
@@ -0,0 +1,131 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { NoteAlt } from "@mui/icons-material";
+import Photo from '../../assets/mcd_logo.png'
+
+
+const Container = styled.div`
+background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba(235, 180, 180, 0.47);
+  padding: 20px;
+  margin: 50px 0;
+`;
+const UserDeatils = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const UserImg = styled.img`
+  width: 30px;
+  height: 30px;
+  object-fit: cover;
+`;
+const Title = styled.h3`
+font-size: 16px;
+color: #4a4b4c;
+margin-bottom: 15px;
+`;
+const P = styled.p`
+color: #8c9ea9;
+font-size: 13px;
+margin-bottom: 24px;
+`;
+const Span = styled.span`
+color: #33cdff;
+`;
+const Username = styled.p``;
+
+const TransactionSearch = ({searchUsers}) => {
+
+  return (
+    <Container>
+        <Title>Search Result(s)</Title>
+        <P>Total Result <Span>{searchUsers.length}</Span></P>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow style={{ backgroundColor: "#f3f2f7" }}>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                Username
+              </TableCell>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                Email
+              </TableCell>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                Phone NUmber
+              </TableCell>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                Wallet Value
+              </TableCell>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                User Group
+              </TableCell>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                Reg Date
+              </TableCell>
+              <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                Action
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {searchUsers &&
+              searchUsers
+                .map((row) => (
+                  <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      <UserDeatils>
+                        <UserImg src={row.photo || Photo} alt="" />
+                        <Username>{row.user_name}</Username>
+                      </UserDeatils>
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.email}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.phoneno}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.wallet}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.status}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      {row.req_date}
+                    </TableCell>
+                    <TableCell style={{ color: "#8887a9" }}>
+                      <Link
+                        to={"/profile/" + encodeURIComponent(row.user_name)}
+                      >
+                        <NoteAlt
+                          style={{
+                            backgroundColor: "5dd099",
+                            color: "white",
+                            padding: "4px 8px",
+                            fontSize: "40px",
+                            cursor: "pointer",
+                          }}
+                        />
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+          </TableBody>
+        </Table>
+        
+      </TableContainer>
+    </Container>
+  );
+};
+
+export default TransactionSearch;
--- src/pages/transaction/FindTransaction.jsx
+++ src/pages/transaction/FindTransaction.jsx
@@ -7,67 +7,96 @@ import {
   Search,
   SimCardDownload,
 } from "@mui/icons-material";
-import styled from "styled-components";
+import { CircularProgress } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../../components/Bar/Navbar";
 import Footer from "../../components/footer/Footer";
-import { laptop, mobile } from "../../responsive";
+import TransactionSearch from "../../components/Transactiontables/TransactionSearch";
+import useInput from "../../Hooks/use-form";
+import { getSearchedUsers } from "../../Redux/apiCalls";
+import {
+  Btn,
+  Container,
+  Form,
+  FormWrapper,
+  Input,
+  InputContainer,
+  Wrapper,
+} from "./findTrans.styles";
+import { Loading } from "./pending.styles";
 import { Desc, DescP, DescSpan, H3 } from "./transHistory.styles";
 
-const Container = styled.div`
-margin: 70px 0;
-${laptop({ marginLeft: "250px" })};
-`;
-const Wrapper = styled.div`
-  padding: 20px;
-  height: 74vh;
-`;
-const FormWrapper = styled.form`
-  background-color: #fff;
-  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
-  padding: 20px 30px;
-`;
-const Form = styled.form`
-  display: grid;
-  grid-template-columns: repeat(2, 50%);
-  gap: 16px;
-  ${mobile({ gridTemplateColumns: "auto" })};
-`;
-const InputContainer = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: #e9ecef;
-  width: 100%;
-  border-radius: 1px;
-  border: 1px solid #e9ecef;
-`;
-const Input = styled.input`
-  padding: 12px;
-  width: 100%;
-  outline-color: #d4d8e1;
-  border: 1px solid #d4d8e1;
-`;
-const Btn = styled.button`
-display: flex;
-align-items: center;
-background: linear-gradient(to right, #5c62ae, #4088a9);
-padding: 10px;
-margin: 20px 0;
-gap: 2px;
-color: white;
-cursor: pointer;
-border-radius: 5px;
-border: none;
-font-weigth: 700;
-font-size: 14px;
-&:hover {
-  color: #343a40;
-  transition: all 0.5s;
-}
-`;
-
 const FindTransaction = () => {
+  const {
+    value: enteredUsername,
+    valueChangeHandler: usernameInputChange,
+    reset: resetNameInput,
+  } = useInput();
+
+  const {
+    value: enteredPhonenumber,
+    valueChangeHandler: phonenumberInputChange,
+    reset: resetPhonenumberInput,
+  } = useInput();
+
+  const {
+    value: enteredTransRef,
+    valueChangeHandler: TransRefInputChange,
+    reset: resetGroupInput,
+  } = useInput();
+
+  const {
+    value: enteredWallet,
+    valueChangeHandler: WalletInputChange,
+    reset: resetWalletInput,
+  } = useInput();
+
+  const {
+    value: enteredTransType,
+    valueChangeHandler: transTypenputChange,
+    reset: resetEmailInput,
+  } = useInput();
+
+  const {
+    value: enteredDate,
+    valueChangeHandler: dateInputChange,
+    reset: resetDateInput,
+  } = useInput();
+
+  const dispatch = useDispatch();
+  const { searchUsers, fecthedUsers, isFetching } = useSelector(
+    (state) => state.profile
+  );
+
+  const allInputValues =
+    enteredUsername ||
+    enteredTransType ||
+    enteredPhonenumber ||
+    enteredDate ||
+    enteredWallet;
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    if (allInputValues.trim() === "") {
+      return getSearchedUsers(dispatch, enteredUsername, enteredPhonenumber);
+    }
+    getSearchedUsers(dispatch, enteredUsername, enteredPhonenumber);
+    resetNameInput();
+    resetPhonenumberInput();
+    resetGroupInput();
+    resetWalletInput();
+    resetEmailInput();
+    resetDateInput();
+  };
+
+  if (isFetching) {
+    return (
+      <Loading>
+        <CircularProgress style={{ color: "blue" }} />
+      </Loading>
+    );
+  }
+
   return (
     <Container>
       <Navbar />
@@ -78,50 +107,80 @@ const FindTransaction = () => {
             Transactions / <DescSpan>Search Transactions</DescSpan>
           </DescP>
         </Desc>
-        <FormWrapper>
+        <FormWrapper onSubmit={handleFormSubmission}>
           <Form>
             <InputContainer>
               <PermIdentity
                 style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              <Input type="text" placeholder="Search for username" />
+              <Input
+                type="text"
+                placeholder="Search for username"
+                onChange={usernameInputChange}
+                value={enteredUsername}
+              />
             </InputContainer>
             <InputContainer>
               <SimCardDownload
                 style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              <Input type="number" placeholder="Search for phone number" />
+              <Input
+                type="number"
+                placeholder="Search for phone number"
+                onChange={phonenumberInputChange}
+                value={enteredPhonenumber}
+              />
             </InputContainer>
             <InputContainer>
               <BusinessCenter
                 style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              <Input type="text" placeholder="Search Transaction Reference" />
+              <Input
+                type="text"
+                placeholder="Search Transaction Reference"
+                onChange={TransRefInputChange}
+                value={enteredTransRef}
+              />
             </InputContainer>
             <InputContainer>
               <AccountBalanceWallet
                 style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              <Input type="text" placeholder="Search for amount" />
+              <Input
+                type="text"
+                placeholder="Search for amount"
+                onChange={WalletInputChange}
+                value={enteredWallet}
+              />
             </InputContainer>
             <InputContainer>
               <Class
                 style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              <Input type="text" placeholder="Search for transaction type" />
+              <Input
+                type="text"
+                placeholder="Search for transaction type"
+                onChange={transTypenputChange}
+                value={enteredTransType}
+              />
             </InputContainer>
             <InputContainer>
               <EventNote
                 style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              <Input type="date" />
+              <Input
+                type="date"
+                onChange={dateInputChange}
+                value={enteredDate}
+              />
             </InputContainer>
           </Form>
           <Btn type="submit">
             <Search />
-            Search
+            {isFetching ? "Searching...." : "Search"}
           </Btn>
         </FormWrapper>
+        {fecthedUsers && <TransactionSearch searchUsers={searchUsers} />}
       </Wrapper>
 
       <Footer />
