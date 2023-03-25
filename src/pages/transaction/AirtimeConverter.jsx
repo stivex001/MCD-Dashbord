d2b18bac9e14d565af1eec802fd65022ced105ea
stivex001 <stephenadeyemo@gmail.com>
2023-03-25 23:03:00 +0100

    fix:code refactored

--- src/pages/transaction/AirtimeConverter.jsx
+++ src/pages/transaction/AirtimeConverter.jsx
@@ -1,82 +1,28 @@
 import { CreditCard } from "@mui/icons-material";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
-import styled from "styled-components";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from "@mui/material";
 import Navbar from "../../components/Bar/Navbar";
 import Footer from "../../components/footer/Footer";
 import { airtimeData } from "../../components/Transactiontables/transactions";
 import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
-
-const Container = styled.div`
-margin: 70px 0;
-`;
-const Wrapper = styled.div`
-  padding: 20px;
-`;
-const FormWrapper = styled.div`
-  background-color: #fff;
-  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
-  padding: 20px 30px;
-`;
-const Form = styled.form`
-  display: flex;
-  align-items: center;
-`;
-const InputContainer = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: #e9ecef;
-  width: 100%;
-  border-radius: 1px;
-`;
-const Input = styled.input`
-  padding: 12px;
-  width: 100%;
-  outline-color: #605daf;
-`;
-const Btn = styled.div`
-  display: flex;
-  align-items: center;
-
-  background-color: #4e74ab;
-  padding: 10px;
-  margin: 20px 0;
-  gap: 2px;
-  color: white;
-  cursor: pointer;
-  border-radius: 1px;
-  &:hover {
-    color: #343a40;
-  }
-`;
-const Button = styled.button`
-  border: none;
-  background: none;
-  color: white;
-  font-weigth: 700;
-  font-size: 14px;
-  cursor: pointer;
-  &:hover {
-    color: #343a40;
-  }
-`;
-const TableWrapper = styled.div`
-  background-color: #fff;
-  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
-  padding: 20px 30px;
-`;
-const Details = styled.div`
-  margin: 30px 0;
-`;
-const Span = styled.span`
-  color: #fff;
-  padding: 2.6px 4.2px;
-  font-size: 10.5px;
-  border-radius: 5px;
-`;
+import {
+  Btn,
+  Container,
+  Details,
+  Form,
+  FormWrapper,
+  Input,
+  InputContainer,
+  Span,
+  TableWrapper,
+  Wrapper,
+} from "./airtimeConverter.styles";
 
 const AirtimeConverter = () => {
   return (
@@ -86,91 +32,110 @@ const AirtimeConverter = () => {
         <Desc>
           <H3>Airtime Converter</H3>
           <DescP>
-            Transaction / <DescSpan>Verification &gt; Airtime Converter</DescSpan>
+            Transaction /{" "}
+            <DescSpan>Verification &gt; Airtime Converter</DescSpan>
           </DescP>
         </Desc>
         <FormWrapper>
-      <Form>
-        <InputContainer>
-          <p style={{ padding: "5px", fontSize: "20px", color: "#495057" }}>
-            #
-          </p>
-          <Input type="text" placeholder="Enter Reference Number" />
-        </InputContainer>
-        <Btn>
-          <CreditCard />
-          <Button>Credit</Button>
-        </Btn>
-      </Form>
-    </FormWrapper>
-      </Wrapper>
-
-      <TableWrapper>
-        <Details>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableBody>
-                <TableRow style={{ backgroundColor: "#f3f2f7" }}>
-                  <TableCell style={{ color: "#8887a9" }}>#</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Reference</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Amount</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Network</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Phone Number</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Status</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Username</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Receiver</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>Date</TableCell>
-                  <TableCell style={{ color: "#8887a9" }}>App Version</TableCell>
-                </TableRow>
-              </TableBody>
-              {airtimeData.map((row) => (
+          <Form>
+            <InputContainer>
+              <p style={{ padding: "5px", fontSize: "20px", color: "#495057" }}>
+                #
+              </p>
+              <Input
+                type="text"
+                placeholder="Enter transaction id or reference"
+                required
+                // onChange={IdInputChange}
+                // value={enteredId}
+              />
+            </InputContainer>
+            <Btn type="submit">
+              <CreditCard />
+              Credit
+            </Btn>
+          </Form>
+        </FormWrapper>
+        <TableWrapper>
+          <Details>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 600 }} aria-label="simple table">
                 <TableBody>
-                  <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
-                    <TableCell style={{ color: "#8887a9" }}>{row.id}</TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.ref}
+                  <TableRow style={{ backgroundColor: "#f3f2f7" }}>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>#</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                      Reference
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.amount}
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>Amount</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>Network</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                      Phone Number
                     </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.network}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.phone}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      <Span
-                        style={{
-                          backgroundColor: `${
-                            row.staus === "Credit" ? "#5dd099" : "#f8c955"
-                          }`,
-                        }}
-                      >
-                        {row.staus}
-                      </Span>
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.username}
-                    </TableCell>
-
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.receiver}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.date}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.version}
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>Status</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>Username</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>Receiver</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>Date</TableCell>
+                    <TableCell style={{ color: "#8281cc", fontWeight: "bold" }}>
+                      App Version
                     </TableCell>
                   </TableRow>
                 </TableBody>
-              ))}
-            </Table>
-          </TableContainer>
-        </Details>
-      </TableWrapper>
+                {airtimeData.map((row) => (
+                  <TableBody>
+                    <TableRow
+                      key={row.id}
+                      style={{
+                        backgroundColor:
+                          row.id % 2 === 0 ? "#ffffff" : "#f3f2f7",
+                      }}
+                    >
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.id}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.ref}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.amount}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.network}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.phone}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        <Span
+                          style={{
+                            backgroundColor: `${
+                              row.staus === "Credit" ? "#5dd099" : "#f8c955"
+                            }`,
+                          }}
+                        >
+                          {row.staus}
+                        </Span>
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.username}
+                      </TableCell>
 
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.receiver}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.date}
+                      </TableCell>
+                      <TableCell style={{ color: "#8887a9" }}>
+                        {row.version}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                ))}
+              </Table>
+            </TableContainer>
+          </Details>
+        </TableWrapper>
+      </Wrapper>
       <Footer />
     </Container>
   );
---
+++ src/pages/transaction/airtimeConverter.styles.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import { laptop } from "../../responsive";
+
+export const Container = styled.div`
+  margin: 70px 0;
+  ${laptop({ marginLeft: "250px" })};
+`;
+export const Wrapper = styled.div`
+  padding: 20px;
+`;
+export const FormWrapper = styled.div`
+  background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px 30px;
+  margin-bottom: 40px;
+`;
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+`;
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #e9ecef;
+  width: 100%;
+  border-radius: 1px;
+`;
+export const Input = styled.input`
+  padding: 12px;
+  width: 100%;
+  outline-color: #d4d8e1;
+  border: 1px solid #d4d8e1;
+`;
+export const Btn = styled.button`
+  display: flex;
+  align-items: center;
+  background: linear-gradient(to right, #5c62ae, #4088a9);
+  padding: 9px;
+  margin: 20px 0;
+  gap: 2px;
+  color: white;
+  cursor: pointer;
+  border-radius: 3px;
+  border: none;
+  font-weigth: 700;
+  font-size: 14px;
+  &:hover {
+    color: #343a40;
+    transition: all 0.5s;
+  }
+`;
+
+export const TableWrapper = styled.div`
+  background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
+  padding: 20px 30px;
+`;
+export const Details = styled.div`
+  margin: 30px 0;
+`;
+export const Span = styled.span`
+  color: #fff;
+  padding: 2.6px 4.2px;
+  font-size: 10.5px;
+  border-radius: 5px;
+`;
