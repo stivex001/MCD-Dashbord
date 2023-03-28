4908d91a11e6132402ee772a39b48b9a5a4c987c
stivex001 <stephenadeyemo@gmail.com>
2023-03-28 22:37:08 +0100

    fix: code refactored

--- src/components/User/UserSearch.jsx
+++ src/components/User/UserSearch.jsx
@@ -5,51 +5,26 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { NoteAlt } from "@mui/icons-material";
-import Photo from '../../assets/mcd_logo.png'
+import Photo from "../../assets/mcd_logo.png";
+import { Container } from "./users.styles";
+import {
+  P,
+  Span,
+  Title,
+  UserDeatils,
+  UserImg,
+  Username,
+} from "./userSearch.styles";
 
-
-const Container = styled.div`
-background-color: #fff;
-  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -moz-box-shadow: 2px 4px 10px 1pxrgba(235, 180, 180, 0.47);
-  padding: 20px;
-  margin: 50px 0;
-`;
-const UserDeatils = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-`;
-const UserImg = styled.img`
-  width: 30px;
-  height: 30px;
-  object-fit: cover;
-`;
-const Title = styled.h3`
-font-size: 16px;
-color: #4a4b4c;
-margin-bottom: 15px;
-`;
-const P = styled.p`
-color: #8c9ea9;
-font-size: 13px;
-margin-bottom: 24px;
-`;
-const Span = styled.span`
-color: #33cdff;
-`;
-const Username = styled.p``;
-
-const UserSearch = ({searchUsers}) => {
-
+const UserSearch = ({ searchUsers }) => {
   return (
     <Container>
-        <Title>Search Result(s)</Title>
-        <P>Total Result <Span>{searchUsers.length}</Span></P>
+      <Title>Search Result(s)</Title>
+      <P>
+        Total Result <Span>{searchUsers.length}</Span>
+      </P>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -79,50 +54,46 @@ const UserSearch = ({searchUsers}) => {
           </TableHead>
           <TableBody>
             {searchUsers &&
-              searchUsers
-                .map((row) => (
-                  <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      <UserDeatils>
-                        <UserImg src={row.photo || Photo} alt="" />
-                        <Username>{row.user_name}</Username>
-                      </UserDeatils>
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.email}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.phoneno}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.wallet}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.status}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      {row.req_date}
-                    </TableCell>
-                    <TableCell style={{ color: "#8887a9" }}>
-                      <Link
-                        to={"/profile/" + encodeURIComponent(row.user_name)}
-                      >
-                        <NoteAlt
-                          style={{
-                            backgroundColor: "5dd099",
-                            color: "white",
-                            padding: "4px 8px",
-                            fontSize: "40px",
-                            cursor: "pointer",
-                          }}
-                        />
-                      </Link>
-                    </TableCell>
-                  </TableRow>
-                ))}
+              searchUsers.map((row) => (
+                <TableRow key={row.id} style={{ backgroundColor: "#f3f2f7" }}>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    <UserDeatils>
+                      <UserImg src={row.photo || Photo} alt="" />
+                      <Username>{row.user_name}</Username>
+                    </UserDeatils>
+                  </TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    {row.email}
+                  </TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    {row.phoneno}
+                  </TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    {row.wallet}
+                  </TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    {row.status}
+                  </TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    {row.req_date}
+                  </TableCell>
+                  <TableCell style={{ color: "#8887a9" }}>
+                    <Link to={"/profile/" + row.user_name}>
+                      <NoteAlt
+                        style={{
+                          backgroundColor: "5dd099",
+                          color: "white",
+                          padding: "4px 8px",
+                          fontSize: "40px",
+                          cursor: "pointer",
+                        }}
+                      />
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
-        
       </TableContainer>
     </Container>
   );
---
+++ src/components/User/userSearch.styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background-color: #fff;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1pxrgba(235, 180, 180, 0.47);
+  padding: 20px;
+  margin: 50px 0;
+`;
+export const UserDeatils = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+export const UserImg = styled.img`
+  width: 30px;
+  height: 30px;
+  object-fit: cover;
+`;
+export const Title = styled.h3`
+font-size: 16px;
+color: #4a4b4c;
+margin-bottom: 15px;
+`;
+export const P = styled.p`
+color: #8c9ea9;
+font-size: 13px;
+margin-bottom: 24px;
+`;
+export const Span = styled.span`
+color: #33cdff;
+`;
+export const Username = styled.p``;
