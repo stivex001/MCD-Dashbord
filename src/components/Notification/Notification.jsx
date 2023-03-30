c706151dc30184eb71b22531976ed24d883de970
stivex001 <stephenadeyemo@gmail.com>
2023-03-30 16:38:40 +0100

    fix: typo fixed

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,17 +1,17 @@
 import { NearMe } from "@mui/icons-material";
 import styled from "styled-components";
-import { tablet } from "../../responsive";
+import {  mobile } from "../../responsive";
 import NotificationHistory from "./NotificationHistory";
 
 const Container = styled.div`
   margin: 50px 0;
   display: flex;
   gap: 30px;
-  ${tablet({ flexDirection: "column" })};
+  ${mobile({ flexDirection: "column" })};
 `;
 const Left = styled.div`
-  flex: 1;
-  background-color: #fff;
+flex: 1;
+background-color: #fff;
   box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
@@ -20,28 +20,28 @@ const Left = styled.div`
   width: 100%;
 `;
 const Textarea = styled.textarea`
-  padding: 6px 12px;
-  outline-color: #333265;
-  ${tablet({ width: "100%", padding: "15px" })};
+padding: 6px 12px;
+outline-color: #333265;
+${mobile({ width: "100%", padding: '15px' })};
 `;
 const Btn = styled.button`
-  display: flex;
-  align-items: center;
-  margin: 5px 0 20px;
-  padding: 6px 12px;
-  color: #fff;
-  background: linear-gradient(to right, #5c62ae, #4088a9);
-  cursor: pointer;
-  border: none;
-  border-radius: 2px;
-  &:hover {
-    color: #343a40;
-    transition: all 0.5s;
-  }
+display: flex;
+align-items: center;
+margin: 5px 0 20px;
+padding: 6px 12px;
+color: #fff;
+background: linear-gradient(to right, #5c62ae, #4088a9);
+cursor: pointer;
+border: none;
+border-radius: 2px;
+&:hover {
+  color: #343a40;
+  transition: all 0.5s;
+}
 `;
 const Right = styled.div`
-  flex: 3;
-  background-color: #fff;
+flex:3;
+background-color: #fff;
   box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
@@ -64,11 +64,11 @@ const Nofication = () => {
         </Btn>
       </Left>
       <Right>
-        <Title>Push Notification History</Title>
-        <NotificationHistory />
+      <Title>Push Notification History</Title>
+      <NotificationHistory />
       </Right>
     </Container>
-  );
-};
+  )
+}
 
-export default Nofication;
+export default Nofication
