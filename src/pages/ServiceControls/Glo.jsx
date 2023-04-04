b7acdcf07d1af092302c1f4ec6faede0ce793ceb
stivex001 <stephenadeyemo@gmail.com>
2023-04-04 14:23:43 +0100

    feat: integrated glo data with backend data

--- src/pages/ServiceControls/Glo.jsx
+++ src/pages/ServiceControls/Glo.jsx
@@ -1,34 +1,83 @@
-import { Paper, Table, TableContainer } from "@mui/material";
-import styled from "styled-components";
+import { PermIdentity, Search, SimCardDownload } from "@mui/icons-material";
+import {
+  CircularProgress,
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../../components/Bar/Navbar";
-import { gloData } from "../../components/DataPlansTable/dataPlans";
 import Tablebody from "../../components/DataPlansTable/Tablebody";
 import Tablehead from "../../components/DataPlansTable/Tablehead";
 import Footer from "../../components/footer/Footer";
+import useInput from "../../Hooks/use-form";
+import {getGloList } from "../../Redux/apiCalls";
+import { Loading } from "../transaction/pending.styles";
 import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
+import {
+  Btn,
+  Container,
+  Details,
+  Form,
+  FormWrapper,
+  Input,
+  InputContainer,
+  P,
+  TableWrapper,
+  Wrapper,
+} from "./airtel.styles";
 
-const Container = styled.div`
-margin: 70px 0;
-`;
-const Wrapper = styled.div`
-  padding: 20px;
-`;
-const TableWrapper = styled.div`
-  background-color: #fff;
-  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
-  -moz-box-shadow: 2px 4px 10px 1pxrgba (235, 180, 180, 0.47);
-  padding: 20px 30px;
-`;
-const P = styled.p`
-  font-size: 13px;
-  color: #8c9ea9;
-`;
-const Details = styled.div`
-  margin: 30px 0;
-`;
+const Airtel = () => {
+  const { gloList, isFetching } = useSelector((state) => state.datalist);
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-const Glo = () => {
+  const {
+    value: enteredType,
+    valueChangeHandler: typeInputChange,
+    reset: resetTypeInput,
+  } = useInput();
+
+  const {
+    value: enteredServer,
+    valueChangeHandler: serverInputChange,
+    reset: resetServerInput,
+  } = useInput();
+
+  const allInputValues = enteredType || enteredServer;
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    if (allInputValues.trim() === "") {
+      return getGloList(dispatch, enteredType, enteredServer);
+    }
+    getGloList(dispatch, enteredType, enteredServer);
+    resetTypeInput();
+    resetServerInput();
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  if (isFetching) {
+    return (
+      <Loading>
+        <CircularProgress style={{ color: "blue" }} />
+      </Loading>
+    );
+  }
+
   return (
     <Container>
       <Navbar />
@@ -39,44 +88,99 @@ const Glo = () => {
             Services / <DescSpan>Data Plans</DescSpan>
           </DescP>
         </Desc>
-      </Wrapper>
-      <TableWrapper>
-        <P>Data Plans</P>
-        <Details>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <Tablehead
-                id="id"
-                network="Network"
-                product="Product Name"
-                price="Provider Price"
-                yourPrice="Your Price"
-                server="Server"
-                status="Status"
-                date="Date Modified"
-                action="Action"
+
+        <FormWrapper onSubmit={handleFormSubmission}>
+          <Form>
+            <InputContainer>
+              <PermIdentity
+                style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
               />
-              {gloData.map((row) => (
-                <Tablebody
-                  id={row.id}
-                  network={row.network}
-                  name={row.name}
-                  price={row.price}
-                  yourprice={row.yourprice}
-                  server={row.server}
-                  action={row.action}
-                  status={row.status}
-                  date={row.date}
-                />
-              ))}
-            </Table>
-          </TableContainer>
-        </Details>
-      </TableWrapper>
+              <Input
+                type="text"
+                placeholder="Search by type (CG, DG, SME)"
+                onChange={typeInputChange}
+                value={enteredType}
+              />
+            </InputContainer>
+            <InputContainer>
+              <SimCardDownload
+                style={{ padding: "5px", fontSize: "30px", color: "#495057" }}
+              />
+              <Input
+                type="number"
+                placeholder="Search by server (1 - 13)"
+                onChange={serverInputChange}
+                value={enteredServer}
+              />
+            </InputContainer>
+          </Form>
+          <Btn type="submit">
+            <Search />
+            {isFetching ? "Searching...." : "Search"}
+          </Btn>
+        </FormWrapper>
+
+        {gloList && (
+          <TableWrapper>
+            <P>Data Plans</P>
+            <Details>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <Tablehead
+                    id="id"
+                    network="Network"
+                    product="Product Name"
+                    price="Provider Price"
+                    yourPrice="Your Price"
+                    server="Server"
+                    status="Status"
+                    date="Date Modified"
+                    action="Action"
+                  />
+                  {gloList.data && gloList.data.length === 0 ? (
+                    <TableRow style={{ backgroundColor: "#f3f2f7" }}>
+                      <TableCell
+                        colSpan={10}
+                        style={{ textAlign: "center", color: "#8887a9" }}
+                      >
+                        No data in the table
+                      </TableCell>
+                    </TableRow>
+                  ) : (
+                    gloList.data &&
+                    gloList.data.map((row) => (
+                      <Tablebody
+                        key={row.id}
+                        id={row.id}
+                        network={row.network}
+                        name={row.name}
+                        price={row.price}
+                        yourprice={row.amount}
+                        server={row.server}
+                        status={row.status}
+                        date={row.updated_at}
+                      />
+                    ))
+                  )}
+                </Table>
+              </TableContainer>
+            </Details>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={gloList?.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableWrapper>
+        )}
+      </Wrapper>
 
       <Footer />
     </Container>
   );
 };
 
-export default Glo;
+export default Airtel;
