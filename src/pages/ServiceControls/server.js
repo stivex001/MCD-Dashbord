954abdf8eeb92c137ea811a51eaa6efcab0a8e19
stivex001 <stephenadeyemo@gmail.com>
2023-04-04 14:56:39 +0100

    fix: changed the inputs to dropdown

---
+++ src/pages/ServiceControls/server.js
@@ -0,0 +1,56 @@
+const Server = [
+  {
+    id: 1,
+    value: 1,
+  },
+  {
+    id: 2,
+    value: 2,
+  },
+  {
+    id: 3,
+    value: 3,
+  },
+  {
+    id: 4,
+    value: 4,
+  },
+  {
+    id: 5,
+    value: 5,
+  },
+  {
+    id: 6,
+    value: 6,
+  },
+  {
+    id: 7,
+    value: 7,
+  },
+  {
+    id: 8,
+    value: 8,
+  },
+  {
+    id: 9,
+    value: 9,
+  },
+  {
+    id: 10,
+    value: 10,
+  },
+  {
+    id: 11,
+    value: 11,
+  },
+  {
+    id: 12,
+    value: 12,
+  },
+  {
+    id: 13,
+    value: 13,
+  },
+];
+
+export default Server;
