763c853be6ad63b81731419a4685a18f63076740
stivex001 <stephenadeyemo@gmail.com>
2023-04-10 16:41:06 +0100

    feat: added stylings

--- src/pages/Reports/Daily.jsx
+++ src/pages/Reports/Daily.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Navbar from "../../components/Bar/Navbar";
+import Footer from "../../components/footer/Footer";
+import { Desc, DescP, DescSpan, H3 } from "../transaction/transHistory.styles";
+
+import {
+  Container,
+  Wrapper,
+} from "../transaction/general.styles";
 
 const Daily = () => {
   return (
-    <div>Daily</div>
-  )
-}
+    <Container>
+      <Navbar />
+      <Wrapper>
+        <Desc>
+          <H3>Daily Report</H3>
+          <DescP>
+            Reports / <DescSpan>Daily Report</DescSpan>
+          </DescP>
+        </Desc>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
 
-export default Daily
+export default Daily;
